fdbc288b3180c94ae12fb8845a37876299e98114
Jelle Vermandere <jelle.vermandere@kuleuven.be>
2022-11-08 16:48:13 +0100

    Image Edge detection

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -38,12 +38,17 @@ Primitives can form a great basis for the generic shape segmentation, however, m
 
 - Genrates a binary tree of clusters
 - allows dynamic selection of the number of clusters
+- edge cutting
+- decimation of the mesh 
+- converting to implicit geometry as end goal for interaction
 
 ## Texture integration
 Textures can provide extra detail and clarify seperations in places of small geometric changes. 
 > Problem: object edges are not always on polygon edges
 - Use vertex colors as extra features in the descision making
 - Create face cuts on texture edges to create texture based polygon edges.
+- compare cosine distance
+
 
 ## Gameplan
 Set up a modular workflow with interchangable methods.
@@ -58,4 +63,16 @@ The next step is determining the treshold for clusters
   - Feature treshold
   - Primitive fitting
 
+## Key Points
+- Focus on hierachical segmentation to allow interaction on multiple scales
+- get greater geometric detail from the texture cuts
+- Use the segmentation method as a stepping stone to the completion
 
+## todo
+- meshnetwerk
+- clustering van de faces, via locale en buren eigenschappen
+- weinig kans op intersectie tussen objecten
+- decimation + subdivision
+- spelen met primitivenet
+- partoon detectie (vb. baksteen)
+- uv mapping chunk seperation
--- segmentationtools/__init__.py
+++ segmentationtools/__init__.py
@@ -1,6 +1,17 @@
 import open3d as o3d
 import matplotlib
 import numpy as np
+from matplotlib import pyplot as plt
+from typing import List
+import cv2
+import math
+
+
+def show_img(img, switchChannels: bool = False):
+    if(switchChannels):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(img)
+    plt.show()
 
 def show_geometries(geometries : 'List[o3d.geometry]', color : bool = False):
     """Displays different types of geometry in a scene
@@ -22,3 +33,182 @@ def show_geometries(geometries : 'List[o3d.geometry]', color : bool = False):
     opt.background_color = np.asarray([1,1,1])
     opt.light_on = True
     viewer.run()
+
+
+def detect_edges(img: np.array) -> np.array:
+    """Finds straight lines in the images and converts them to parametric polar lines
+
+    Args:
+        img (cv2.image): The input image.
+
+    Returns:
+        np.array: an array of parametric polar lines
+    """
+ 
+    # First detect the edges using Canny detection to convert the image to a bw edge image.
+    dst = cv2.Canny(img, 50, 200, None, 3)
+
+    # Use Standard HoughLine transform to detect parametric lines explressed in polar coordinates (rho, theta)
+    lines = cv2.HoughLines(dst, 1, np.pi / 180, 150, None, 0, 0)
+    return lines
+
+
+
+def get_point_on_polar_line(line: np.array, x:float = None, y:float = None) -> float:
+    """Returns the corresponding value of a single axis value on a polar line
+
+    Args:
+        line (np.array): The polar line [[rho,theta]]
+        x (float, optional): The X coordinate, leave empty if you want to use the y value. Defaults to None.
+        y (float, optional): The Y coordinate. Defaults to None.
+
+    Returns:
+        float: the corresponding x or y value
+        None: if no value was provided
+    """
+
+    rho = line[0][0]
+    theta = line[0][1]
+    cost = math.cos(theta)
+    sint = math.sin(theta)
+    a = -cost/sint # the slope
+    b = rho/sint # the constant
+
+    if(x is not None):
+        return a * x + b
+    elif(y is not None):
+        return (y - b) / a
+    else:
+        return None
+
+def get_edge_points(line: np.array, xMax: float, yMax: float) -> np.array:
+    """Returns intersections between a line and a bounds area
+
+    Args:
+        line (np.array): The polar line to use for the intersection [[rho,theta]]
+        xMax (float): the max x value of the bounds
+        yMax (float): the max y value of the bounds
+
+    Returns:
+        np.array: the array of intersectiong points along the edges
+    """
+
+    points = []
+
+    # check the min x value
+    y0 = get_point_on_polar_line(line, x = 0)
+    if(y0 > 0 and y0 < yMax):
+        points.append(np.array([0,y0]))
+
+    # check the max x value
+    y1 = get_point_on_polar_line(line, x = xMax)
+    if(y1 > 0 and y1 < yMax):
+        points.append(np.array([xMax,y1]))
+
+    # check the min y value
+    x0 = get_point_on_polar_line(line, y = 0)
+    if(x0 > 0 and x0 < xMax):
+        points.append(np.array([x0,0]))
+
+    # check the max y value
+    x1 = get_point_on_polar_line(line, y = yMax)
+    if(x1 > 0 and x1 < xMax):
+        points.append(np.array([x1,yMax]))
+        
+    if(len(points) == 0): 
+        return None
+    return np.array(points)
+
+def line_intersection(line1: np.array, line2: np.array) -> np.array:
+    """Calculates an instesection point between 2 lines defined by a pair of points
+
+    Args:
+        line1 (np.array): The first line as a couple of points [[xy],[xy]]
+        line2 (np.array): The second line as a couple of points [[xy],[xy]]
+
+    Returns:
+        np.array: The resulting intersection point
+        None: if the lines are parallel 
+    """
+
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+       return None
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return np.array((x, y))
+
+def check_bounds(point:np.array, bounds:np.array) -> bool:
+    """Checks if a point lies withing a bounding area defined by a couple of line points
+
+    Args:
+        point (np.array): The point to test
+        bounds (np.array): The points defined as a array of 2 points [[xy],[xy]]
+
+    Returns:
+        bool: if it's inside the bounds or not
+    """
+
+    bounds = np.round(np.array(bounds),1)
+    minx = np.min(bounds[:,0])
+    maxx = np.max(bounds[:,0])
+    miny = np.min(bounds[:,1])
+    maxy = np.max(bounds[:,1])
+
+    roundPoint = np.round(point,1)
+    if(roundPoint[0] >= minx and roundPoint[0] <= maxx and roundPoint[1] >= miny and roundPoint[1] <= maxy):
+        return True
+    return False
+
+def line_segment_intersection(line1: np.array,line2:np.array) -> np.array:
+    """Calculates an instesection point between 2 line segments defined by a pair of points
+
+    Args:
+        line1 (np.array): The first line segment as a couple of points [[xy],[xy]]
+        line2 (np.array): The second line segment as a couple of points [[xy],[xy]]
+
+    Returns:
+        np.array: The resulting intersection point
+        None: if the lines are parallel or not instersecting
+    """
+
+    point = line_intersection(line1,line2)
+    if(check_bounds(point, line1) and check_bounds(point, line2)):
+        return point
+    return None
+
+def find_triangle_intersection(edgePoints: np.array, line: np.array) -> np.array:
+    """Finds intersecting points between the edges of a triangle and a line
+
+    Args:
+        edgePoints (np.array): The 3 points of the triangle [[xy],[xy],[xy]]
+        line (np.array): The intersection line as a couple of points [[xy],[xy]]
+
+    Returns:
+        np.array: An array of the intersection points
+        None: If there are no intersections
+    """
+    
+    tLine1 = ((edgePoints[0][0], edgePoints[0][1]), (edgePoints[1][0], edgePoints[1][1]))
+    tLine2 = ((edgePoints[1][0], edgePoints[1][1]), (edgePoints[2][0], edgePoints[2][1]))
+    tLine3 = ((edgePoints[2][0], edgePoints[2][1]), (edgePoints[0][0], edgePoints[0][1]))
+
+    points = []
+    point1 = line_segment_intersection(tLine1, line)
+    if(point1 is not None): points.append(point1)
+    point2 = line_segment_intersection(tLine2, line)
+    if(point2 is not None): points.append(point2)
+    point3 = line_segment_intersection(tLine3, line)
+    if(point3 is not None): points.append(point3)
+
+    if(len(points) == 0): 
+        return None
+    return np.array(points)
